2710067e72ab0e1a7603ff36e8c6c9189ae570bb
Anshul Chaudhary <141403895+Anshulchoudhary0@users.noreply.github.com>
2026-05-05 16:47:52 +0000

    add

--- JSadvance/1st.js
+++ JSadvance/1st.js
@@ -1,10 +1,10 @@
 //const accountId = 144553
 //let accountEmail = "hitesh@google.com"
-// var accountPassword = "12345"
+// key word (var) accountPassword = "12345"
 // accountCity = "Jaipur"
-// let accountState;
+// let accountState; undefined for uninitialized value or value not assigned
 
-// accountId = 2 // not allowed for const
+// accountId = 2 // not allowed for const or not change for const variable
 
 
 // accountEmail = "hc@hc.com"
@@ -12,7 +12,7 @@
 // accountCity = "Bengaluru"
 
 // console.log(accountId);
-
+ 
 /*
 Prefer not to use var
 because of issue in block scope and functional scope
@@ -31,10 +31,10 @@ because of issue in block scope and functional scope
 // console.log("Hitesh")
 
 
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
+//let name = "hitesh"
+//let age = 18
+//let isLoggedIn = false
+//let state;
 
 // number => 2 to power 53
 // bigint for larger numbers 
@@ -42,7 +42,7 @@ let state;
 // boolean => true/false for logical operations
 // null => standalone value for unknown value or empty value
 // undefined => for uninitialized value or value not assigned
-// symbol => unique
+// symbol => unique for creating unique identifiers for objects
 
 /* primitive data types => number, string, boolean, null, undefined, symbol
 non-primitive data types => object (array, function, etc)
@@ -50,7 +50,53 @@ non-primitive data types => object (array, function, etc)
 // object
 
 // console.log(typeof undefined); // undefined for uninitialized value or value not assigned
-console.log(typeof (null)); // object because of legacy reasons, but it is a primitive data type
-console.log(typeof name); // string
-console.log(typeof age); // number
-console.log(typeof isLoggedIn); // boolean  
+// console.log(typeof (null)); // object because of legacy reasons, but it is a primitive data type
+// console.log(typeof name); // string
+// console.log(typeof age); // number
+// console.log(typeof isLoggedIn); // boolean  
+ 
+
+
+
+// data type conversion
+
+// let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+// it give string for both cases, because score is a string data type
+
+// let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//  now it give number for both cases, because we have converted score to number data type, but it give NaN because "hitesh" is not a number
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+//  and if we put null at the place of score then it will give 0 because null is considered as 0 in number data type
+
+// let isLoggedIn = "hitesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+// if i paste 1 and 0 in the place of hitesh
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+ 
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let score = "33";
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
---
+++ JSadvance/practice.js
@@ -0,0 +1,5 @@
+let score = "33"
+let Number = something
+let valueInNumber = Number(score) // ✅ correct
+
+console.log(typeof valueInNumber) // number
